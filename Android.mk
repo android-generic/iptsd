840eb7c035f9f01ed77b5185d0ab00a69527c59b
hmtheboy154 <buingoc67@gmail.com>
2021-01-02 21:40:46 +0700

    Adding Android.mk

---
+++ Android.mk
@@ -0,0 +1,67 @@
+LOCAL_PATH:= $(call my-dir)
+
+
+#build iptsd
+include $(CLEAR_VARS)
+INIH_INCLUDE := $(LOCAL_PATH)/../inih
+LOCAL_CFLAGS := -Wundef \
+	-Wuninitialized \
+	-Wno-unused-result \
+	-Wmissing-include-dirs \
+	-Wold-style-definition \
+	-Wpointer-arith \
+	-Winit-self \
+	-Wstrict-prototypes \
+	-Wendif-labels \
+	-Wstrict-aliasing=2 \
+	-Woverflow \
+	-Wmissing-prototypes \
+	-Wno-missing-braces \
+	-Wno-missing-field-initializers \
+	-Wno-unused-parameter -std=gnu99
+LOCAL_C_INCLUDES:= $(INIH_INCLUDE) \
+				$(LOCAL_PATH)
+LOCAL_SRC_FILES := 	 src/cone.c  \
+	src/config.c  \
+	src/contact.c  \
+	src/control.c  \
+	src/data.c  \
+	src/devices.c  \
+	src/finger.c  \
+	src/heatmap.c  \
+	src/hid.c  \
+	src/main.c  \
+	src/reader.c  \
+	src/payload.c  \
+	src/singletouch.c  \
+	src/stylus.c  \
+	src/touch.c  \
+	src/touch-processing.c  \
+	src/utils.c
+LOCAL_MODULE := iptsd
+LOCAL_SHARED_LIBRARIES := libinih
+include $(BUILD_EXECUTABLE)
+
+#build iptsd-dbg
+include $(CLEAR_VARS)
+LOCAL_CFLAGS := -Wundef \
+	-Wuninitialized \
+	-Wno-unused-result \
+	-Wmissing-include-dirs \
+	-Wold-style-definition \
+	-Wpointer-arith \
+	-Winit-self \
+	-Wstrict-prototypes \
+	-Wendif-labels \
+	-Wstrict-aliasing=2 \
+	-Woverflow \
+	-Wmissing-prototypes \
+	-Wno-missing-braces \
+	-Wno-missing-field-initializers \
+	-Wno-unused-parameter -std=gnu99
+LOCAL_C_INCLUDES:= $(LOCAL_PATH)
+LOCAL_SRC_FILES := debug/debug.c \
+	src/control.c  \
+	src/utils.c
+LOCAL_MODULE := ipts-dbg
+include $(BUILD_EXECUTABLE)
