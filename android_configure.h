90d89463b3907a81cf1481d0c38e81abc41a322c
hmtheboy154 <buingoc67@gmail.com>
2022-06-04 14:16:22 -0400

    Android : move BINDIR/DATADIR/SYSCONFDIR to /vendor

--- android_configure.h
+++ android_configure.h
@@ -3,11 +3,11 @@
 #ifndef _IPTSD_CONFIGURE_H_
 #define _IPTSD_CONFIGURE_H_
 
-#define IPTSD_BINDIR "/system/bin"
-#define IPTSD_DATADIR "/system/usr/share"
-#define IPTSD_SYSCONFDIR "/system/etc"
+#define IPTSD_BINDIR "/system/vendor/bin"
+#define IPTSD_DATADIR "/system/vendor/usr/share"
+#define IPTSD_SYSCONFDIR "/system/vendor/etc"
 
-#define IPTSD_CONFIG_DIR "/system/etc/ipts"
+#define IPTSD_CONFIG_DIR "/system/vendor/etc/ipts"
 #define IPTSD_CONFIG_FILE "ipts.conf"
 
 #endif /* _IPTSD_CONFIGURE_H_ */
