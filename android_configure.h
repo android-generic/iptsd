fc5f6488006cdaa63d5abbefe84cc28b98b70234
hmtheboy154 <buingoc67@gmail.com>
2024-05-26 05:43:43 -0400

    Update Android.mk & android_configure.h
    Add -frtti to avoid build error and also add IPTSD_FORCE_ACCESS_CHECKS
    to build config
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- android_configure.h
+++ android_configure.h
@@ -1,10 +1,8 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 
-#ifndef IPTSD_CONFIGURE_H
-#define IPTSD_CONFIGURE_H
+#pragma once
 
 #define IPTSD_PRESET_DIR "/vendor/etc/ipts"
-#define IPTSD_CONFIG_DIR "/data/vendor/ipts"
-#define IPTSD_CONFIG_FILE "/vendor/etc/ipts.conf"
-
-#endif /* _IPTSD_CONFIGURE_H_ */
+#define IPTSD_CONFIG_DIR "/data/vendor/iptsd.d"
+#define IPTSD_CONFIG_FILE "/data/vendor/iptsd.conf"
+#define IPTSD_FORCE_ACCESS_CHECKS 0
